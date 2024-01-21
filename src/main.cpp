5c8b84f88d8c946b07ff5fb88f8e8088a5a8f16a
Francisco Gasca <francisco_gasca@hotmail.com>
2024-01-21 08:17:29 -0600

    Set a random seed at start and print it

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include "Canvas.h"
+#include <cstdlib>
+#include <cstdio>
 
 int main()
 {
+    long seed;
+    srand(time(&seed));
+    printf("seed: %ld\n", seed);
+
     Canvas canvas;
 
     canvas.start();
