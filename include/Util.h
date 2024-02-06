b2a4ff9634e7d32748adb4239792278042baf3f5
Francisco Gasca <francisco_gasca@hotmail.com>
2024-02-06 09:07:56 -0600

    Add map function

--- include/Util.h
+++ include/Util.h
@@ -9,6 +9,11 @@ inline int random(int lower, int upper)
     return (rand() % (upper - lower + 1)) + lower;
 }
 
+inline float map(float value, float min1, float max1, float min2, float max2)
+{
+    return min2 + (max2 - min2) * ((value - min1) / (max1 - min1));
+}
+
 int randomInRange(std::vector<int> values);
 bool isNumber(char* text, long* value);
 
