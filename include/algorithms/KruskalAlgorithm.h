69bf8675e026cba6aad9c0593ea5c66568bcbae6
Francisco Gasca <francisco_gasca@hotmail.com>
2024-02-11 21:01:49 -0600

    Implement the Kruskal Algorithm as it should be (Much better optimized btw)

--- include/algorithms/KruskalAlgorithm.h
+++ include/algorithms/KruskalAlgorithm.h
@@ -6,10 +6,33 @@
 
 struct Edge
 {
-    Edge(int y, int x, int wall);
+    Edge(int y, int x, int wall, int weight);
     int x;
     int y;
     int wall;
+    int weight;
+};
+
+struct Node
+{
+    Node(int parent, int rank);
+    // In this case, the parent represents its index in the DisjointSet nodes array
+    int parent;
+    int rank;
+};
+
+class DisjointSet
+{
+public:
+    DisjointSet(int height, int width);
+    // Get the root of a node
+    int find(int node);
+    int find(int nodeY, int nodeX);
+    void setUnion(int n1, int n2);
+private:
+    // Note: this actually represents a bidimensional array
+    std::vector<Node> nodes;
+    int width; // To calculate index with two values later
 };
 
 class KruskalAlgorithm : public Algorithm
@@ -21,8 +44,9 @@ public:
     float calculatePercentage() override;
 private:
     size_t initialWallsSize;
-    std::vector<Edge> walls;
-    std::vector<std::vector<std::set<std::shared_ptr<Cell>>>> sets;
+    std::vector<Edge> edges; // More appropiate name
+    // Pointer to initialize later
+    std::unique_ptr<DisjointSet> disjointSet;
 };
 
 #endif
--- src/algorithms/KruskalAlgorithm.cpp
+++ src/algorithms/KruskalAlgorithm.cpp
@@ -1,58 +1,115 @@
 #include <vector>
 #include <set>
+#include <algorithm>
 
 #include "algorithms/KruskalAlgorithm.h"
 #include "util/Util.h"
 
-Edge::Edge(int y, int x, int wall): x(x), y(y), wall(wall)
+Edge::Edge(int y, int x, int wall, int weight): x(x), y(y), 
+    wall(wall), weight(weight)
 {}
+
+Node::Node(int parent, int rank): parent(parent), rank(rank)
+{}
+
+DisjointSet::DisjointSet(int height, int width) : width(width)
+{
+    for (int y = 0; y < height; y++)
+    {
+        std::vector<Node> row;
+        for (int x = 0; x < width; x++)
+        {
+            // since each node is the first element of its set, is its own parent (root)
+            nodes.emplace_back(y * width + x, 0);
+        }
+    }    
+}
+
+int DisjointSet::find(int node)
+{
+    if (nodes[node].parent != node)
+    {
+        // Path compression
+        nodes[node].parent = find(nodes[node].parent);
+        return nodes[node].parent;
+    }
+    
+    return node;
+}
+
+int DisjointSet::find(int nodeY, int nodeX)
+{
+    return find(nodeY * width + nodeX);
+}
+
+void DisjointSet::setUnion(int n1, int n2)
+{
+    // This may be unnecessary because where the function is called, this
+    // has already been done, but I want to keep the function "independent" 
+    n1 = find(n1);
+    n2 = find(n2);
+
+    if (n1 == n2)
+    {
+        return;
+    }
+
+    if (nodes[n1].rank < nodes[n2].rank)
+    {
+        // Swap nodes to ensure that n1 has the larger rank tree
+        int tmp = n1;
+        n1 = n2;
+        n2 = tmp;
+    }
+    else if (nodes[n1].rank == nodes[n2].rank)
+    {
+        nodes[n1].rank++;
+    }
+
+    nodes[n2].parent = n1; 
+
+}
 
 void KruskalAlgorithm::start(std::function<void()> callback)
 {
+    random.setSeed(seed);
+
     for (int y = 0; y < maze.getHeight(); y++)
     {
         for (int x = 0; x < maze.getWidth(); x++)
         {
+            // Add only inner edges (between (1,1) and (height - 1, width - 1))
+            // And set a random weight to them
             if (y > 0)
             {
-                walls.emplace_back(y, x, Wall::UP);
+                edges.emplace_back(y, x, Wall::UP, random.get());
             }
 
             if (x > 0)
             {
-                walls.emplace_back(y, x, Wall::LEFT);
+                edges.emplace_back(y, x, Wall::LEFT, random.get());
             }
         }
     }
 
-    initialWallsSize = walls.size();
+    initialWallsSize = edges.size();
 
-    for (int i = 0; i < maze.getHeight(); i++)
-    {
-        std::vector<std::set<std::shared_ptr<Cell>>> row;
-        for (int n = 0; n < maze.getWidth(); n++)
-        {
-            std::set<std::shared_ptr<Cell>> setCell;
-            setCell.insert(maze.getCells()[i][n]);
+    // Sort in descending order to pop the elements when they are used
+    // (only to calculate the percentage with the edges size)
+    std::sort(edges.begin(), edges.end(), [](Edge e1, Edge e2) { return e1.weight > e2.weight; });
+    disjointSet = std::make_unique<DisjointSet>(maze.getHeight(), maze.getWidth());
 
-            row.push_back(std::set<std::shared_ptr<Cell>>(setCell));
-        }
-
-        sets.push_back(row);
-    }
-
-    random.setSeed(seed);
     calculate(-1, -1, callback);
 }
 
 void KruskalAlgorithm::calculate(int y, int x, std::function<void()> callback)
 {
     std::vector<std::vector<std::shared_ptr<Cell>>>& cells = maze.getCells();
-    while (!walls.empty())
+
+    while (!edges.empty())
     {
-        int i = random.getInRange(0, walls.size() - 1);
-        Edge edge = walls[i];
-        walls.erase(walls.begin() + i);
+        Edge& edge = edges.back();
+        edges.pop_back();
 
         cells[edge.y][edge.x]->setVisited();
         cells[edge.y][edge.x]->setSurrounded();
@@ -70,23 +127,12 @@ void KruskalAlgorithm::calculate(int y, int x, std::function<void()> callback)
             callback();
         }
 
-        std::set<std::shared_ptr<Cell>>& set1 = sets[edge.y][edge.x];
-        std::set<std::shared_ptr<Cell>>& set2 = sets[edge.y + dy][edge.x + dx];
+        int n1 = disjointSet->find(edge.y, edge.x);
+        int n2 = disjointSet->find(edge.y + dy, edge.x + dx);
 
-        if (set1 != set2)
+        if (n1 != n2)
         {
-            set1.insert(set2.begin(), set2.end());
-            
-            std::set<std::shared_ptr<Cell>>::iterator it;
-            for (it = set1.begin(); it != set1.end(); ++it)
-            {
-                std::shared_ptr<Cell> cell = *it; 
-
-                int y = cell->getY();
-                int x = cell->getX();
-
-                sets[y][x] = set1;
-            }
+            disjointSet->setUnion(n1, n2);
 
             maze.setWall(edge.y, edge.x, edge.wall, false);
 
@@ -102,5 +148,5 @@ void KruskalAlgorithm::calculate(int y, int x, std::function<void()> callback)
 
 float KruskalAlgorithm::calculatePercentage()
 {
-    return map(walls.size(), initialWallsSize, 0, 0.f, 100.f);
+    return map(edges.size(), initialWallsSize, 0, 0.f, 100.f);
 }
